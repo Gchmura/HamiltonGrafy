03e6b851ed677ad1ff3974f04c8e83ddbfd9209e
Grzegorz Chmura <gchmura3@gmail.com>
2018-03-20 14:53:27 +0100

    pawel commit

---
+++ C#/HamiltonianCircuit/Models/ConstsValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamiltonianCircuit.Models
+{
+    public abstract class ConstsValues
+    {
+        public const int percentageProbability = 90;
+    }
+}
---
+++ C#/HamiltonianCircuit/Models/RandomService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamiltonianCircuit.Models
+{
+    public static class RandomService
+    {
+        private static Random rand = new Random();
+        
+        public static int GetNoOfEdges(int vertex)
+        {
+            //Properties.Settings.Default.Probability
+            var probability = rand.Next(100);
+            int numberOfEdges = 0;
+            for (int i = 0; i < vertex; i++)
+            {
+                if (probability <= ConstsValues.percentageProbability)
+                {
+                    i++;
+                }
+              
+            }
+
+            return numberOfEdges;
+            
+
+        }
+    }
+}
